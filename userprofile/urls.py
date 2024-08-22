19dc8c507e2884ca3d29a5bcdc6104de34da6c50
Kaoru <kaoru@gmail.com>
2024-08-22 21:17:47 +0800

    新增view-userprofile與view-userprofile.html

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("view-userprofile/", views.view_userprofile, name="view-userprofile"),
     path("create-userprofile/", views.create_userprofile, name="create-userprofile"),
+    path("edit-userprofile/", views.edit_userprofile, name="edit-userprofile"),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import UserProfile
 from .forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
@@ -6,19 +6,32 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required
+def view_userprofile(request):
+    try:
+        userprofile = UserProfile.objects.get(user=request.user)
+
+    except UserProfile.DoesNotExist:
+        return redirect("create-userprofile")
+
+    return render(
+        request, "userprofile/view-userprofile.html", {"userprofile": userprofile}
+    )
+
+
 # 建立個人資料
 @login_required
 def create_userprofile(request):
     message = ""
-    userprofileform = None
 
     if request.method == "POST":
         form = UserProfileForm(request.POST)
+
         if form.is_valid():
             userprofileform = form.save(commit=False)
             userprofileform.user = request.user
             userprofileform.save()
-            message = "建立個人資訊成功"
+            return redirect("view-userprofile")
 
         else:
             message = "資料錯誤"
@@ -28,5 +41,38 @@ def create_userprofile(request):
     return render(
         request,
         "userprofile/create-userprofile.html",
-        {"form": form, "userprofileform": userprofileform, "message": message},
+        {"form": form, "message": message},
+    )
+
+
+# 修改個人資料
+@login_required
+def edit_userprofile(request):
+    message = ""
+
+    try:
+        userprofile = UserProfile.objects.get(user=request.user)
+
+    except UserProfile.DoesNotExist:
+        return redirect("create-userprofile")
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=userprofile)
+
+        if form.is_valid():
+            userprofileform = form.save(commit=False)
+            userprofileform.user = request.user
+            userprofileform.save()
+            return redirect("view-userprofile")
+
+        else:
+            message = "資料錯誤"
+
+    else:
+        form = UserProfileForm(instance=userprofile)
+
+    return render(
+        request,
+        "userprofile/edit-userprofile.html",
+        {"message": message, "form": form},
     )
