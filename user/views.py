7f12b5d07e1934b275c5f9fc1de7849ef661e809
Kaoru <kaoru@gmail.com>
2024-08-22 14:27:17 +0800

    新增base.html，style.css

--- user/views.py
+++ user/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def user_register(request):
+    message = ""
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        else:
+            message = "資料錯誤"
+
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request, "user/user-register.html", {"form": form, "message": message}
+    )
