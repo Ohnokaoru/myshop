ca71da308a832e9eeef5b62937828c561a6e2de7
Kaoru <kaoru@gmail.com>
2024-08-23 10:58:52 +0800

    新增produtc model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_price = models.IntegerField(default=0)
+    product_img = models.ImageField(upload_to="product_images/", null=True, blank=True)
+    product_description = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return f"{self.product_name} {self.product_price}"
