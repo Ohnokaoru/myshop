437f7c437e73ff62f22269c2a79818d30980f1c1
Kaoru <kaoru@gmail.com>
2024-08-26 18:19:10 +0800

    add_cart設定庫存不足message

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("add-cart/<int:product_id>", views.add_cart, name="add-cart"),
+    path("view-cart/", views.view_cart, name="view-cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import CartItem
 from product.models import Product
-from django.core.exceptions import ValidationError
+
 
 # Create your views here.
 
@@ -17,15 +17,34 @@ def add_cart(request, product_id):
     except Product.DoesNotExist:
         return redirect("index")
 
+    # 庫存
+    if product.product_stock == 0:
+        message = "目前缺貨中"
+
     try:
         cart_item = CartItem.objects.get(user=request.user, product=product)
         cart_item.quantity += 1
-        cart_item.save()
-        message = "商品已在購物車，數量+1"
+        if cart_item.quantity == product.product_stock:
+            message = "添加數量已經最大囉!"
+        else:
+            cart_item.save()
+            message = "商品已在購物車，數量+1"
 
+    # 手動增加(不使用form)
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(user=request.user, product=product)
         cart_item.quantity = 1
         message = "新增商品到購物車"
 
     return redirect("index")
+
+
+# 檢視購物車內容
+@login_required
+def view_cart(request):
+    cart_items = CartItem.objects.all()
+
+    if len(cart_items) == 0:
+        return redirect("index")
+
+    return render(render, "cart/view-cart.html", {"cart_items": cart_items})
